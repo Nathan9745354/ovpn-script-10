597d27b4cbf574fc412b5c95f55883db7125cb36
Nathan9745354 <46953407+Nathan9745354@users.noreply.github.com>
2022-02-18 02:22:35 +0800

    Add files via upload

---
+++ ovpn-script-10.sh
@@ -0,0 +1,81 @@
+#!/bin/bash 
+# Author by Prosfinity Limited
+
+
+wget -O speedtest-cli https://raw.github.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+
+apt-get install python
+./speedtest-cli
+
+echo "-------------------------------"
+echo "SpeedTest Done"
+echo "-------------------------------"
+
+
+wget https://raw.githubusercontent.com/Nathan9745354/openvpn-ubuntu-install/main/openvpn-ubuntu-install.sh
+chmod 777 openvpn-ubuntu-install.sh
+./openvpn-ubuntu-install.sh
+
+echo "-------------------------------"
+echo "OpenVPN Installation Done"
+echo "-------------------------------"
+
+
+echo "duplicate-cn" >> /etc/openvpn/server/server.conf
+echo "status /var/log/openvpn-status.log" >> /etc/openvpn/server/server.conf
+echo "log-append /var/log/openvpn.log" >> /etc/openvpn/server/server.conf
+echo "max-clients 10" >> /etc/openvpn/server/server.conf
+sed -i 's/server 10.8.0.0 255.255.255.0/server 10.8.0.0 255.255.255.240/' /etc/openvpn/server/server.conf
+
+
+echo "-------------------------------"
+echo "Server.conf Config Done"
+echo "-------------------------------"
+cat /etc/openvpn/server/server.conf
+
+
+cd /home
+mkdir ubuntu
+cd ubuntu
+
+wget https://raw.githubusercontent.com/Nathan9745354/gen100user.sh/main/gen100user.sh
+chmod 777 gen100user.sh
+sed -i -e 's/\r$//' gen100user.sh
+./gen100user.sh
+
+cd /root
+mv user* /home/ubuntu
+
+
+echo "-------------------------------"
+echo "Generate 100 users Done"
+echo "-------------------------------"
+
+
+iptables -S
+
+iptables -L
+
+iptables -D FORWARD -s 10.8.0.0/24 -j ACCEPT
+
+iptables -A FORWARD -s 10.8.0.0/28 -j ACCEPT
+
+iptables -S
+
+echo "-------------------------------"
+echo "IPtables Config Done"
+echo "-------------------------------"
+
+
+exit
+
+
+
+
+
+
+
+
+
+
